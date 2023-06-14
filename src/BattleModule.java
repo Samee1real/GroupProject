2fc5dc3ab8553a054e2245bf7904339d574cf333
Samee1real <g.sameenath@gmail.com>
2023-06-13 22:57:18 -0400

    basic stuff

---
+++ src/BattleModule.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Samee
+ */
+public class BattleModule {
+                                            //Module Variables\\
+    
+    public boolean active; //True if in battle | Setting to false will pretty much disable everything
+    
+                                            //Placement Order\\
+    
+    public Unit[] placementOrder = new Unit[8]; 
+    
+                                             //Setup Methods\\
+    
+    public void LoadUnits() //Future Method
+    {
+    
+    }
+    
+    public void StartBattle()
+    {
+        
+    }
+    
+    public void CloseBattle()
+    {
+    
+    }
+    
+                                           //Battle Methods\\
+    
+    public void ScrambleOrder()
+    {
+        if (active) 
+        {
+            
+        }
+    }
+    
+    public void ExecutiveMove(String move, Unit unit)
+    {
+        if (active)
+        {
+            
+        }
+    }
+}
---
+++ src/TurnModule.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Samee
+ */
+public class TurnModule {
+                                //Module Variables\\
+    private int round;
+    
+                                   //Turn Order\\
+    private Unit[] turnOrder = new Unit[8];
+    
+}
