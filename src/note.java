86c4d71e6700b8a23017b8fdddc3f8ca3b3b6b46
Samee1real <99507789+Samee1real@users.noreply.github.com>
2023-06-20 14:53:56 -0400

    Update note.java
    vffrffgg

--- src/note.java
+++ src/note.java
@@ -46,4 +46,49 @@ public class note {
             button.setText(finalText);
         }
     }
+    private static ArrayList<Point> labelLocations = new ArrayList<battlePlaces>();
+    private static ArrayList<JLabel> positionLabels = new ArrayList<battlePlaces>(); //if u do labelOrder this will be useless
+    private static ArrayList<Unit> labelUnits = new ArrayList<battlePlaces>();    //index 0 is label 1
+    //HAVE A LABEL ARRAYLIST JUST LKE PLACE ORDER, WHENEVER PLACEORDER IS UPDATED, THIS IS ALSO UPDATED (labelOrder)
+
+    //swap
+    /* get position from place Order
+    TweenLabelTask(labelOrder.get(pos1), labelLocations.get(pos2), dur);
+    TweenLabelTask(labelOrder.get(pos2), labelLocations.get(pos1), dur);
+    */
+    
+    public static class TweenLabelTask extends Thread
+    {
+        private JLabel label;
+        Point goal;
+        double dur; 
+        
+        public TweenLabelTask(javax.swing.JLabel label, Point point, double dur)
+        {
+            this.label = label; this.dur = dur;
+            goal = point;
+            Systm.out.println("goal x is " + goal.x + " given has " + point.x);
+        }
+        public void run()
+        {
+            System.out.println("STARTED");
+            double time = 0;
+            Point start = label.getLocation();
+            System.out.println("start is  " +  start);
+            while (time < dur) {
+                //Incrementing/Waiting Time
+                try {
+                    Thread.sleep((long)easingThreadSpeed);    //Waiting thread time
+                    time += easingThreadSpeed;  //This will increase time to update it
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(UIManager.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                //Updating location
+                double easeTime = easeStyles("Quad", time/dur);
+                int x = start.x + (int)((goal.x-start.x)*easeTime);
+                int y = start.y + (int)((goal.y-start.y)*easeTime);
+                label.setLocation(x, y);    
+            }
+        }
+    }
 }
